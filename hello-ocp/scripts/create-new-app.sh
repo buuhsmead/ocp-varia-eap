17916240bf336bfa1fbd30fad0d023dcc6e517b4
Huub Daems <buuhsmead@gmail.com>
2019-01-22 19:58:17 +0100

    Thanks to https://github.com/travisrogers05/eap-oracle-db

--- hello-ocp/scripts/create-new-app.sh
+++ hello-ocp/scripts/create-new-app.sh
@@ -2,9 +2,17 @@
 
 ## oc describe template eap71-basic-s2i  -n openshift
 
+oc set env dc/hello-ocp ENV_FILES=/opt/eap/standalone/configuration/*.env
 
 ## This is an EAR so ARTIFACT_DIR has to be set !
-oc new-app --template=eap71-basic-s2i --param-file=create-new-app.params.env --param-file=database.params.env
+oc new-app --template=eap71-basic-s2i \
+  -p APPLICATION_NAME=hello-ocp \
+  -p SOURCE_REPOSITORY_URL="https://github.com/buuhsmead/ocp-varia-eap" \
+  -p SOURCE_REPOSITORY_REF="master" \
+  -p CONTEXT_DIR="hello-ocp" \
+  -p ARTIFACT_DIR="hello-ocp-ear/target" \
+  -p SCRIPT_DEBUG="true"
+
 
 
 ## oc logs -f bc/hello-ocp
